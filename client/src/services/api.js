63d6abbcc5d13c39cdf86e43d9cc569a7ffa3e61
suneater003 <aditya1raj003@gmail.com>
2026-05-29 08:14:16 +0530

    frontend-bugfix-for deployement

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
-});
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 api.interceptors.request.use((config) => {
   if (typeof window !== 'undefined') {
